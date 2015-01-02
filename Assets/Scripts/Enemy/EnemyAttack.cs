5bcbed3a54836c8d0b1bee4a8043b9de02b9a031
Salihan04 <salihan04@gmail.com>
2015-01-02 15:40:30 +0800

    Update player health upon damage. Enable enemy to attack. Red flash on screen when player takes damage.

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour 
+{
+	public float timeBetweenAttacks = 0.5f;
+	public int attackDamage = 10;
+
+	private Animator anim;
+	private GameObject player;
+	private PlayerHealth playerHealth;
+	private bool playerInRange;
+	private float timer;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerHealth = player.GetComponent<PlayerHealth>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject == player)
+			playerInRange = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject == player)
+			playerInRange = false;
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= timeBetweenAttacks && playerInRange)
+			Attack();
+
+		if(playerHealth.currentHealth <= 0)
+			anim.SetTrigger("PlayerDead");
+	}
+
+	void Attack()
+	{
+		timer = 0f;
+
+		if(playerHealth.currentHealth > 0)
+			playerHealth.TakeDamage(attackDamage);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public AudioClip deathClip;
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color(1f, 0f, 0f, 1f);
+
+	private Animator anim;
+	private AudioSource playerAudio;
+	private PlayerMovement playerMovement;
+	private bool isDead;
+	private bool damaged;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+		playerAudio = GetComponent<AudioSource>();
+		playerMovement = GetComponent<PlayerMovement>();
+		currentHealth = startingHealth;
+	}
+
+	void Update()
+	{
+		if(damaged)
+			damageImage.color = flashColor;
+		else
+			damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+
+		damaged = false;
+	}
+
+	public void TakeDamage(int amount)
+	{
+		damaged = true;
+
+		currentHealth -= amount;
+
+		healthSlider.value = currentHealth;
+
+		playerAudio.Play();
+
+		if(currentHealth <= 0 && !isDead)
+			Death();
+	}
+
+	void Death()
+	{
+		//To make sure health slider is fully red when the player is dead
+		GameObject healthFill = GameObject.FindGameObjectWithTag("HealthFill");
+//		Destroy(healthFill);
+
+		Image image = healthFill.GetComponentInChildren<Image>();
+		image.enabled = false;
+
+		isDead = true;
+
+		anim.SetTrigger("Die");
+
+		playerAudio.clip = deathClip;
+		playerAudio.Play ();
+
+		playerMovement.enabled = false;
+	}
+}
