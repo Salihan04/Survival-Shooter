457bbe23dcff4bf50ef6cb53669fa1d7a9c40ea8
Salihan04 <salihan04@gmail.com>
2015-01-05 12:20:34 +0800

    Created enemy spawn points

--- Assets/Scripts/Manager/EnemyManager.cs
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -21,6 +21,6 @@ public class EnemyManager : MonoBehaviour
 
 		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-		Instantiate(enemy, spawnPoints[spawnPointIndex], spawnPoints[spawnPointIndex].rotation);
+		Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 	}
 }
